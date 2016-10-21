9505118b6a80ff366208d52f46f8d1a0e26964ae
Amey Kamat <amkama@microsoft.com>
2016-10-21 15:08:07 -0700

    Initial Commit

---
+++ base_script.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+git --version 
+
+echo "This is the base script"
+
+echo "Cluster: ${Cluster}.azurehdinsight.net"
+
+ClusterSSH=${Cluster}-ssh.azurehdinsight.net
+echo "Cluster ssh name: ${ClusterSSH}"
+
+echo "ssh username: ${ClusterUsername}"
+echo "ssh password: ${ClusterPassword}"
+
+echo "Database selected: ${DatabaseSize}"
+
+echo "Cloning the scripts repo ..."
+
+git clone <git repo for scripts>
+cd <the script repo>
+
+echo "Copying the hive script over to the cluster..."
+
+sshpass -p ${ClusterPassword} scp -o "StrictHostKeyChecking no" ${ClusterUsername}@${ClusterSSH}:/home/${ClusterUsername}/
+
+sshpass -p ${ClusterPassword} ssh -o "StrictHostKeyChecking no" ${ClusterUsername}@${ClusterSSH} chmod a+x /home/${ClusterUsername}/hive_script.sh
+
+sshpass -p ${ClusterPassword} ssh -o "StrictHostKeyChecking no" ${ClusterUsername}@${ClusterSSH} /home/${ClusterUsername}/hive_script.sh ${ClusterUsername} ${DatabaseSize}
+
+if [ $? -ne 0 ]; then
+	echo "Failed in hive_script. Exiting ..."
+	exit 1
+fi
+
+echo "The hive_script completed!! Collecting the results..."
+sshpass -p ${ClusterPassword} scp -o "StrictHostKeyChecking no" ${ClusterUsername}@${ClusterSSH}:/home/${ClusterUsername}/hive-testbench/logs/query_times.csv .
+cat query_times.csv
---
+++ hive_script.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Setting some params
+ClusterUsername=$1
+DatabaseSize=$2
+
+echo "Cloning the hive-testbench"
+# git clone https://github.com/hdinsight/HivePerformanceAutomation /home/${ClusterUsername}/hive-testbench
+
+testbench=/home/${ClusterUsername}/hive-testbench
+
+chmod -R 777 $testbench
+chmod -R a+x $testbench/*.sh
+
+# go to tpch-scripts to run the script
+cd $testbench/tpch-scripts
+echo "Running the queries ... "
+./RunQueries.sh ${DatabaseSize}
+
+
+
+
+
