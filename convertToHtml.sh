2d1843d604085a1f1f617537e1ef4bf60b06915d
Amey Kamat <amkama@microsoft.com>
2016-10-25 12:32:10 -0700

    Removing the relative path

--- convertToHtml.sh
+++ convertToHtml.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+# Setting some params from base_script.sh
+ClusterUsername=$1
+
 echo "<html>" > res.html
 echo "<head/>" >> res.html
 echo "<body>" >> res.html
@@ -5,7 +10,7 @@ echo "<table>" >> res.html
 while read INPUT
 do
         echo "<tr><td>${INPUT//,/</td><td>}</td></tr>"  >> res.html
-done < ~/result.csv
+done < /home/${ClusterUsername}/result.csv
 echo "</table>" >> res.html
 echo "</body>" >> res.html
 echo "</html>" >> res.html
